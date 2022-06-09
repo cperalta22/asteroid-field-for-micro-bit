e1ea159f20b09dea4daefb2b49b13bbeac65e630
Carlos Alberto Peralta Alvarez <drperaltaxp@hotmail.com>
2022-06-09 14:41:08 -0500

    name_correction

--- main.py
+++ main.py
@@ -1,21 +1,59 @@
 # Asteroid Field
 # CPA 2022
 
+# TEST ZONE 
+
+from microbit import *
 from asteriod import *
+from characters import *
 
-while True:
+start_msg()
+sleep(300)
 
-    if button_a.was_pressed():
-       
-        lifes_count = 3
-        level = 0
-        start_msg()
-        life_set(lifes_count)
-        ship_set(2)
-        #while lifes_count > 0:
-        
-    elif button_b.was_pressed():
-       life_set(0)
-       stop_msg()
-    
+xwing = space_object(2,4,9)
+xwing.set_coor()
+tie0 = space_object(0,0,0)
+tie1 = space_object(0,0,0)
+tie2 = space_object(0,0,0)
+tie3 = space_object(0,0,0)
+tie4 = space_object(0,0,0)
+
+score = 0
+vida = 3
+while vida > 0:
+    if tie4.x_pos == xwing.x_pos and tie4.y_pos == xwing.y_pos:
+        display.clear()
+        vida = vida - 1
+        display.scroll(vida)
+        display.show(Image.HEART)
+        sleep(500)
+        display.clear()
+        tie0 = space_object(0,0,0)
+        tie1 = space_object(0,0,0)
+        tie2 = space_object(0,0,0)
+        tie3 = space_object(0,0,0)
+        tie4 = space_object(0,0,0)
+        xwing.set_coor()
+    else:
+        score = score+1
+        tie4.off()
+        tie4 = tie3
+        tie4.attack()
+        tie3 = tie2
+        tie3.attack()
+        tie2 = tie1
+        tie2.attack()
+        tie1 = tie0
+        tie1.attack()
+        tie0 = space_object(random.randint(0,4),0,4)
+        tie0.set_coor()
+        xwing.x_nav(1)
+
+display.show(Image.SAD)
+sleep(500)
+stop_msg(score)
+
+
+            
+
 
